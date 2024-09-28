c01b85314c01e6ae0c21140b115fc504f3dc2ff7
testboram <rkdqhfka7@gmail.com>
2024-09-28 15:10:47 +0900

    a

---
+++ src/main/java/kr/ac/koreatech/devops_step0/HelloController.java
@@ -0,0 +1,15 @@
+package kr.ac.koreatech.devops_step0;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+  @GetMapping("/")
+  public String index() {
+    System.out.println("hello...");
+    return "index.html";
+  }
+  
+}
